b74e5fd2204d782aea2b1c725ca0bc79ccd5aee8
github username <josephtrettevik@gmail.com>
2019-11-05 10:08:10 -0800

    Filled out controllers but still need to test them.

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -3,4 +3,39 @@ class ItemsController < ApplicationController
         @items = Item.all;
         render :json => @items;
     end
+
+    def show
+        @item = Item.find(params[:id])
+        render :json => @item
+    end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        puts item_params
+        @item = Item.new(item_params)
+        @item.save
+        render :json => @item
+    end
+
+    def edit
+        @item = Item.find(params[:id])
+    end
+
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        render :json => @item
+    end
+
+    def destroy
+        @item = Item.find(params[:id])
+        @item.destroy
+    end 
+
+    def item_params
+        params.require(:item).permit(:id, :name, :category, :quantity, :units)
+    end
 end
--- backend/app/controllers/locations_controller.rb
+++ backend/app/controllers/locations_controller.rb
@@ -3,4 +3,39 @@ class LocationsController < ApplicationController
         @locations = Location.all
         render :json => @locations
     end
+
+    def show
+        @location = Location.find(params[:id])
+        render :json => @location
+    end
+
+    def new
+        @location = Location.new
+    end
+
+    def create
+        puts location_params
+        @location = Location.new(location_params)
+        @location.save
+        render :json => @location
+    end
+
+    def edit
+        @location = Location.find(params[:id])
+    end
+
+    def update
+        @location = Location.find(params[:id])
+        @location.update(location_params)
+        render :json => @location
+    end
+
+    def destroy
+        @location = Location.find(params[:id])
+        @location.destroy
+    end 
+
+    def location_params
+        params.require(:location).permit(:id, :name, :address, :lat, :lng, :milesFrom)
+    end
 end
