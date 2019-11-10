685b7062b17241eca3fa98afb33603b91b74a623
github username <josephtrettevik@gmail.com>
2019-11-10 11:34:32 -0800

    generated a match table and the routes work

---
+++ backend/app/models/match.rb
@@ -0,0 +1,4 @@
+class Match < ApplicationRecord
+    belongs_to :user
+    belongs_to :user_items
+end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :items, through: :user_items, dependent: :destroy
     has_many :user_locations 
     has_many :locations, through: :user_locations, dependent: :destroy
+    has_many :matches
 end
