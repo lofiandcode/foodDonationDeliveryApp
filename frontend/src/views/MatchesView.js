eb17575bf0bbd0096c92d1c72a97bc85262149f1
github username <josephtrettevik@gmail.com>
2019-11-14 10:11:01 -0800

    Added the react-geocode npm package and it fixed by geocoding api problem

--- frontend/src/views/MatchesView.js
+++ frontend/src/views/MatchesView.js
@@ -1,8 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import Geocode from "react-geocode";
 require("dotenv").config()
-
 const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+
+// set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+Geocode.setApiKey(REACT_APP_API_KEY);
+ 
+// set response language. Defaults to english.
+Geocode.setLanguage("en");
+
+
+
 const mapStyles = {
     width: '100%',
     height: '100%',
@@ -61,9 +70,9 @@ class MatchesView extends Component {
             newMatchesWithObjsArray.push(newMatchObj)
             console.log('newMatchesWithObjsArray = ', newMatchesWithObjsArray)
         })
-        this.setState({ matchesWithObjs: newMatchesWithObjsArray }, () => this.testCallFunction())
+        this.setState({ matchesWithObjs: newMatchesWithObjsArray }, () => this.setGeocodes())
     }
-    testCallFunction = () => {
+    setGeocodes = () => {
         console.log('IN TEST CALL FUNCTION')
         const matches = [...this.state.matchesWithObjs]
         console.log('matches = ', matches)
@@ -73,9 +82,12 @@ class MatchesView extends Component {
         this.testPrintFunctionDonor( matches[1], 1)
         this.testPrintFunctionDriver( matches[1], 1)
         this.testPrintFunctionFoodBank( matches[1], 1)
-        this.geocodeAddressAndSetStateOfDonor(0, 0)
-        this.geocodeAddressAndSetStateOfDriver(0, 0)
-        this.geocodeAddressAndSetStateOfFoodBank(1, 0)
+        this.state.matchesWithObjs.map((match, idx) => {
+            this.geocodeAddressAndSetStateOfDonor(idx, 0)
+            this.geocodeAddressAndSetStateOfDriver(idx, 0)
+            this.geocodeAddressAndSetStateOfFoodBank(idx, 0)
+            return match
+        })
     }
     testPrintFunctionDonor = (match, idx) => {
         console.log(`objType donor_user_item.user.locations[0].address = `, match.donor_user_item.user.locations[0].address)
@@ -90,53 +102,135 @@ class MatchesView extends Component {
     //     console.log(`objType ${objType}.locations[0].address = `, match[objType].name)
     // }
     // 
-    // displayMarkers = () => {
-    //     console.log('Matches in displayMatches = ', this.state.locations)
-    //     return this.state.locations.map((location, index) => {
-    //         return <Marker key={index} id={index} position={{
-    //          lat: location.lat,
-    //          lng: location.lng
-    //        }}
-    //        onClick={() => console.log("You clicked me!")} />
-    //     })
-    // }
+    
     
     geocodeAddressAndSetStateOfDonor = (match_idx, location_idx) => {
         console.log('IN GECODE DONOR AND STATE ADDRESS RETURNED IS = ', this.state.matchesWithObjs[match_idx].donor_user_item.user.locations[location_idx].address)
+        
+        Geocode.fromAddress(this.state.matchesWithObjs[match_idx].donor_user_item.user.locations[location_idx].address)
+        .then(
+            response => {
+                const { lat, lng } = response.results[0].geometry.location;
+                console.log("IN NEW GECODE. DONOR LAT LNG = ", lat, lng);
+                this.setState(prevState => ({
+                    matchesWithObjs: prevState.matchesWithObjs.map(
+                        (el,idx) => idx === match_idx? { ...el, donorLatLng: {lat, lng}}: el
+                    )
+                }), () => console.log('state.matchesWithObjs = ', this.state.matchesWithObjs))
+            },
+            error => {
+                console.error(error);
+            }
+        );
+    }
+    geocodeAddressAndSetStateOfDriver= (match_idx, location_idx) => {
+        console.log('IN GECODE DRIVER AND STATE ADDRESS RETURNED IS = ', this.state.matchesWithObjs[match_idx].driver.locations[location_idx].address)
+        Geocode.fromAddress(this.state.matchesWithObjs[match_idx].driver.locations[location_idx].address)
+        .then(
+            response => {
+                const { lat, lng } = response.results[0].geometry.location;
+                console.log("IN NEW GECODE. DRIVER LAT LNG = ", lat, lng);
+                this.setState(prevState => ({
+                    matchesWithObjs: prevState.matchesWithObjs.map(
+                        (el,idx) => idx === match_idx? { ...el, driverLatLng: {lat, lng}}: el
+                    )
+                }), () => console.log('state.matchesWithObjs = ', this.state.matchesWithObjs))
+            },
+            error => {
+                console.error(error);
+            }
+        );
+
         // const geocoder = new this.props.google.maps.Geocoder()
-        // geocoder.geocode({address: }, (response, status) => {
+        // geocoder.geocode({address: this.state.matchesWithObjs[match_idx].driver.locations[location_idx].address}, (response, status) => {
+        //     console.log('driver gecode response = ', response)
         //     if (status === 'OK') {
-        //         // console.log('Geocoder lat = ', response[0].geometry.bounds.na.l)
-        //         // console.log('Geocoder lng = ', response[0].geometry.bounds.ga.l)
-        //         this.setState(prevState => {
-        //             return { matchesWithObjs: [...prevState.latlngs, {lat: response[0].geometry.bounds.na.l, lng: response[0].geometry.bounds.ga.l}]
-        //             }
-        //         })
+        //         console.log('Geocoder lat = ', response[0].geometry.viewport.na.l)
+        //         console.log('Geocoder lng = ', response[0].geometry.viewport.ga.l)
+        //         this.setState(prevState => ({
+        //             matchesWithObjs: prevState.matchesWithObjs.map(
+        //                 (el,idx) => idx === match_idx? { ...el, driverLatLng: {lat: response[0].geometry.viewport.na.l, lng: response[0].geometry.viewport.ga.l}}: el
+        //             )
+        //         }), () => console.log('state.matchesWithObjs = ', this.state.matchesWithObjs))
         //     } else {
         //         alert('Geocode was not successful for the following reason: ' + status);
         //     }
         // })
-        
-    }
-    geocodeAddressAndSetStateOfDriver= (match_idx, location_idx) => {
-        console.log('IN GECODE DRIVER AND STATE ADDRESS RETURNED IS = ', this.state.matchesWithObjs[match_idx].driver.locations[location_idx].address)
-        const geocoder = new this.props.google.maps.Geocoder()
-        geocoder.geocode({address: this.state.matchesWithObjs[match_idx].driver.locations[location_idx].address}, (response, status) => {
-            if (status === 'OK') {
-                console.log('Geocoder lat = ', response[0].geometry.bounds.na.l)
-                console.log('Geocoder lng = ', response[0].geometry.bounds.ga.l)
-                this.setState(prevState => ({
-                    matchesWithObjs: prevState.matchesWithObjs.map(
-                        (el,idx) => idx === match_idx? { ...el, driverLatLng: {lat: response[0].geometry.bounds.na.l, lng: response[0].geometry.bounds.ga.l}}: el
-                    )
-                }), () => console.log('state.matchesWithObjs = ', this.state.matchesWithObjs))
-            } else {
-                alert('Geocode was not successful for the following reason: ' + status);
-            }
-        })
     }
     geocodeAddressAndSetStateOfFoodBank = (match_idx, location_idx) => {
         console.log('IN GECODE FOOD BANK AND STATE ADDRESS RETURNED IS = ', this.state.matchesWithObjs[match_idx].foodBank.locations[location_idx].address)
+        Geocode.fromAddress(this.state.matchesWithObjs[match_idx].foodBank.locations[location_idx].address)
+        .then(
+            response => {
+                const { lat, lng } = response.results[0].geometry.location;
+                console.log("IN NEW GECODE. FOOD BANK LAT LNG = ", lat, lng);
+                this.setState(prevState => ({
+                    matchesWithObjs: prevState.matchesWithObjs.map(
+                        (el,idx) => idx === match_idx? { ...el, foodBankLatLng: {lat, lng}}: el
+                    )
+                }), () => console.log('state.matchesWithObjs = ', this.state.matchesWithObjs))
+            },
+            error => {
+                console.error(error);
+            }
+        );
+        // const geocoder = new this.props.google.maps.Geocoder()
+        // geocoder.geocode({address: this.state.matchesWithObjs[match_idx].foodBank.locations[location_idx].address}, (response, status) => {
+        //     console.log('food bank gecode response = ', response)
+        //     if (status === 'OK') {
+        //         console.log('Geocoder lat = ', response[0].geometry.viewport.na.l)
+        //         console.log('Geocoder lng = ', response[0].geometry.viewport.ga.l)
+        //         this.setState(prevState => ({
+        //             matchesWithObjs: prevState.matchesWithObjs.map(
+        //                 (el,idx) => idx === match_idx? { ...el, foodBankLatLng: {lat: response[0].geometry.viewport.na.l, lng: response[0].geometry.viewport.ga.l}}: el
+        //             )
+        //         }), () => console.log('state.matchesWithObjs = ', this.state.matchesWithObjs))
+        //     } else {
+        //         alert('Geocode was not successful for the following reason: ' + status);
+        //     }
+        // })
+    }
+    displayDonorMarkers = () => {
+        // console.log('Matches in displayMatches = ', this.state.locations)
+        console.log('ABOUT TO CHECK FIRST CONDITIONAL AND this.state.matchesWithObjs = ', this.state.matchesWithObjs)
+        if (this.state.matchesWithObjs.length !== 0 ){
+            // debugger
+            console.log('ABOVE TO CHECK CONDITIONAL AND this.state.matchesWithObjs[0].donorLatLng = ',this.state.matchesWithObjs[0].donorLatLng)
+            if (this.state.matchesWithObjs[0].donorLatLng && this.state.matchesWithObjs[1].donorLatLng) {
+                console.log('IN SECOND CONDITIONAL this.state.matchesWithObjs[0].donorLatLng = ', this.state.matchesWithObjs[0].donorLatLng)
+                return this.state.matchesWithObjs.map((match, index) => {
+                    return <Marker key={index} id={index} position={{
+                    lat: match.donorLatLng.lat,
+                    lng: match.donorLatLng.lng
+                }}
+                onClick={() => console.log("You clicked me!")} />
+                })
+            } else {
+                return null
+            }
+        } else {
+            return null
+        }
+    }
+    displayDriverMarkers = () => {
+        // console.log('Matches in displayMatches = ', this.state.locations)
+        return this.state.matchesWithObjs.map((match, index) => {
+            return <Marker key={index} id={index} position={{
+             lat: match.driverLatLng.lat,
+             lng: match.driverLatLng.lng
+           }}
+           onClick={() => console.log("You clicked me!")} />
+        })
+    }
+    displayFoodBankMarkers = () => {
+        // console.log('Matches in displayMatches = ', this.state.locations)
+        return this.state.matchesWithObjs.map((match, index) => {
+            return <Marker key={index} id={index} position={{
+             lat: match.foodBankLatLng.lat,
+             lng: match.foodBankLatLng.lng
+           }}
+           onClick={() => console.log("You clicked me!")} />
+        })
     }
     render() {
         console.log('Matches props = ', this.props)
@@ -148,9 +242,12 @@ class MatchesView extends Component {
                     style={mapStyles}
                     initialCenter={{lat: 47.618475, lng: -122.337975}}
                 >
-                    {/* {this.displayMarkers()} */}
+                    {this.displayDonorMarkers()}
+
+                    {/*this.displayDriverMarkers()*/}
+                    {/*this.displayFoodBankMarkers()*/}
                     {/* <Marker position={{lat: 47.62227910000001, lng: -122.33990849999998}} animation={this.props.google.maps.Animation.DROP}/> */}
-                    <Marker position={{lat: 47.618475, lng: -122.337975}} animation={this.props.google.maps.Animation.DROP}/>
+                    {/* <Marker position={{lat: 47.618475, lng: -122.337975}} animation={this.props.google.maps.Animation.DROP}/> */}
                 </Map>
             </Fragment>
         );
@@ -162,3 +259,9 @@ export default GoogleApiWrapper({
     apiKey: REACT_APP_API_KEY
   })(MatchesView);
 
+//   () => {
+//     console.log('ABOVE TO CHECK CONDITIONAL AND this.state.matchesWithObjs[0].donorLatLng = ',this.state.matchesWithObjs[0].donorLatLng)
+//     if (this.state.matchesWithObjs[0].donorLatLng) {
+//         console.log('IN JXS DONOR IF')
+//     }
+// }
