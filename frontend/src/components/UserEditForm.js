c3aa0537f916b51214d76f5ddf3aa1b61f248c73
github username <josephtrettevik@gmail.com>
2019-12-30 13:55:24 -0800

    started to add a state to UserEditForm

--- frontend/src/components/UserEditForm.js
+++ frontend/src/components/UserEditForm.js
@@ -1,6 +1,31 @@
 import React, {Component} from 'react'
+import {
+    Button,
+    Form,
+    Input,
+    Select,
+    TextArea,
+  } from 'semantic-ui-react'
+  import { withRouter } from 'react-router-dom';
+
+const options = [
+    { key: 'driver', text: 'Driver', value: 'driver' },
+    { key: 'donor', text: 'Donor', value: 'donor' },
+    { key: 'FB', text: 'Food Bank', value: 'food bank' },
+]
 
 class UserEditForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          name: this.props.user.name,
+          username: '',
+          password: '',
+          role: '',
+          phoneNum: '',
+          about: ''
+        }
+    }
     render() {
         // console.log('UserEditForm props = ', this.props)
         if (this.props.user.name) {
@@ -13,7 +38,7 @@ class UserEditForm extends Component {
                         type="text"
                         name='name'
                         onChange={(e) => this.props.handleFormChange(e)}
-                        value={this.props.user.name}
+                        value={this.state.name}
                     /><br/>
                     <label>Account Type:</label>
                     <input
@@ -48,3 +73,28 @@ class UserEditForm extends Component {
 }
 
 export default UserEditForm
+
+// import React from 'react'
+
+
+// const FormExampleWidthField = () => (
+//   <Form>
+//     <Form.Group>
+//       <Form.Input label='First name' placeholder='First Name' width={6} />
+//       <Form.Input label='Middle Name' placeholder='Middle Name' width={4} />
+//       <Form.Input label='Last Name' placeholder='Last Name' width={6} />
+//     </Form.Group>
+//     <Form.Group>
+//       <Form.Input placeholder='2 Wide' width={2} />
+//       <Form.Input placeholder='12 Wide' width={12} />
+//       <Form.Input placeholder='2 Wide' width={2} />
+//     </Form.Group>
+//     <Form.Group>
+//       <Form.Input placeholder='8 Wide' width={8} />
+//       <Form.Input placeholder='6 Wide' width={6} />
+//       <Form.Input placeholder='2 Wide' width={2} />
+//     </Form.Group>
+//   </Form>
+// )
+
+// export default FormExampleWidthField
