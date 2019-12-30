5225fb1fef1e51d8aa2b1272b9b60f83f0d429e3
github username <josephtrettevik@gmail.com>
2019-12-30 15:08:41 -0800

    change form to a semantic-ui form in UserEditFrom

--- frontend/src/components/UserEditForm.js
+++ frontend/src/components/UserEditForm.js
@@ -19,57 +19,119 @@ class UserEditForm extends Component {
         super(props);
         this.state = {
           name: this.props.user.name,
+          username: this.props.user.username,
+          password: this.props.user.password,
           role: this.props.user.role,
           phoneNum: this.props.user.phoneNum,
           about: this.props.user.about
         }
     }
-    handleFormChange = (e) => {
+    handleChange = (e) => {
         e.persist();
         // console.log('IN handleChange')
         // const key = e.target.name;
         this.setState({
           [e.target.name]: e.target.value
         }, () => console.log(`${e.target.name} = `, this.state[e.target.name]))
-      }
+    }
+    handleSubmit = (e) => {
+        this.props.handleCreateAccountSubmit(e, this.state);
+        this.props.history.push('/profile/address')
+    }
     render() {
         // console.log('UserEditForm props = ', this.props)
         if (this.props.user.name) {
             // console.log('IN UserEditForm about to render form')
             return (
-                <div>
-                    <form>
-                        <label>Name:</label>
-                    <input
-                        type="text"
-                        name='name'
-                        onChange={(e) => this.handleFormChange(e)}
-                        value={this.state.name}
-                    /><br/>
-                    <label>Account Type:</label>
-                    <input
-                        type="text"
-                        name='role'
-                        onChange={(e) => this.handleFormChange(e)}
-                        value={this.state.role}
-                    /><br/>
-                    <label>About:</label>
-                    <input
-                        type="text"
+                <Form onSubmit={(e) => this.handleSubmit(e)}>
+                    <Form.Group widths='equal'>
+                        <Form.Field
+                            control={Select}
+                            options={options}
+                            name='role'
+                            value={this.state.role}
+                            onChange={this.handleSelectChange}
+                            label='Account Type'
+                            placeholder='Select Account Type'
+                        />
+                        <Form.Field
+                            control={Input}
+                            name='username'
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                            label='Username'
+                            placeholder='Username'
+                        />
+                        <Form.Field
+                            control={Input}
+                            name='password'
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            label='Password'
+                            placeholder='Password'
+                        />
+                    </Form.Group>
+                    <Form.Group widths='equal'>
+                        <Form.Field
+                            control={Input}
+                            name='name'
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                            label='Individual or Organization Name'
+                            placeholder='Name'
+                        />
+                        <Form.Field
+                            control={Input}
+                            name='phoneNum'
+                            value={this.state.phoneNum}
+                            onChange={this.handleChange}
+                            label='Phone'
+                            placeholder='(xxx) xxx-xxxx'
+                        />
+                    </Form.Group>
+                    <Form.Field
+                        control={TextArea}
                         name='about'
-                        onChange={(e) => this.handleFormChange(e)}
                         value={this.state.about}
-                    /><br/>
-                    <label>Phone Number:</label>
-                    <input
-                        type="text"
-                        name='phoneNum'
-                        onChange={(e) => this.handleFormChange(e)}
-                        value={this.state.phoneNum}
-                    /><br/>
-                    <button type='submit' onSubmit={ () => this.handleSubmit()}>Submit</button>
-                    </form>
-                </div>
+                        onChange={this.handleChange}
+                        label='About'
+                        placeholder='Tell us more about you...'
+                    />
+                    <Form.Field control={Button}>Submit</Form.Field>
+                </Form>
+                // <div>
+                //     <form>
+                //         <label>Name:</label>
+                //     <input
+                //         type="text"
+                //         name='name'
+                //         onChange={(e) => this.handleFormChange(e)}
+                //         value={this.state.name}
+                //     /><br/>
+                //     <label>Account Type:</label>
+                //     <input
+                //         type="text"
+                //         name='role'
+                //         onChange={(e) => this.handleFormChange(e)}
+                //         value={this.state.role}
+                //     /><br/>
+                //     <label>About:</label>
+                //     <input
+                //         type="text"
+                //         name='about'
+                //         onChange={(e) => this.handleFormChange(e)}
+                //         value={this.state.about}
+                //     /><br/>
+                //     <label>Phone Number:</label>
+                //     <input
+                //         type="text"
+                //         name='phoneNum'
+                //         onChange={(e) => this.handleFormChange(e)}
+                //         value={this.state.phoneNum}
+                //     /><br/>
+                //     <button type='submit' onSubmit={ () => this.handleSubmit()}>Submit</button>
+                //     </form>
+                // </div>
             )
         } else {
             // console.log('IN UserEditForm outer else')
@@ -78,29 +140,4 @@ class UserEditForm extends Component {
     }
 }
 
-export default UserEditForm
-
-// import React from 'react'
-
-
-// const FormExampleWidthField = () => (
-//   <Form>
-//     <Form.Group>
-//       <Form.Input label='First name' placeholder='First Name' width={6} />
-//       <Form.Input label='Middle Name' placeholder='Middle Name' width={4} />
-//       <Form.Input label='Last Name' placeholder='Last Name' width={6} />
-//     </Form.Group>
-//     <Form.Group>
-//       <Form.Input placeholder='2 Wide' width={2} />
-//       <Form.Input placeholder='12 Wide' width={12} />
-//       <Form.Input placeholder='2 Wide' width={2} />
-//     </Form.Group>
-//     <Form.Group>
-//       <Form.Input placeholder='8 Wide' width={8} />
-//       <Form.Input placeholder='6 Wide' width={6} />
-//       <Form.Input placeholder='2 Wide' width={2} />
-//     </Form.Group>
-//   </Form>
-// )
-
-// export default FormExampleWidthField
+export default withRouter(UserEditForm)
